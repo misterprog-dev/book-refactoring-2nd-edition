7272982b46d8933d9398d98742dcfd62af8f2508
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-04 13:03:53 +0100

    incoporate variable

--- chapter 1/statement.ts
+++ chapter 1/statement.ts
@@ -8,9 +8,7 @@ export function statement(invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
 
-    let totalAmount = appleSauce();
-
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(appleSauce())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
