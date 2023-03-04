9fdb3d408516360c1bfd713c8297ccd2a7543867
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-04 13:03:29 +0100

    extract function

--- chapter 1/statement.ts
+++ chapter 1/statement.ts
@@ -2,20 +2,26 @@ import * as invoices from "./data/invoices.json";
 import * as plays from "./data/plays.json";
 
 export function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
 
-    for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
-    }
+    let totalAmount = appleSauce();
 
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+
+    function appleSauce() {
+        let totalAmount = 0;
+
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
 
     function totalVolumeCredits() {
         let volumeCredits = 0;
