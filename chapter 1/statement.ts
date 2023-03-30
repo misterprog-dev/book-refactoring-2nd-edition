eac6e21298e14fcc913bee3ce27245be0c8a80eb
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-30 11:54:03 +0200

    move amountfor in statement

--- chapter 1/statement.ts
+++ chapter 1/statement.ts
@@ -15,12 +15,36 @@ export function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result)
+        result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
 
     function playFor(aPerformance: any) {
         return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance: any): number {
+        let result = 0;
+
+        switch (aPerformance.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
+        }
+        return result;
     }
 }
 
@@ -30,7 +54,7 @@ function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
 
     for (let perf of data.performances) {
-        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
     }
 
     result += `Amount owed is ${usd(totalAmount())}\n`;
@@ -41,7 +65,7 @@ function renderPlainText(data, plays) {
         let result = 0;
 
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
@@ -67,29 +91,6 @@ function renderPlainText(data, plays) {
         result += Math.max(aPerformance.audience - 30, 0);
         if ("comedy" === aPerformance.play.type) {
             result += Math.floor(aPerformance.audience / 5);
-        }
-        return result;
-    }
-
-    function amountFor(aPerformance: any): number {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
         return result;
     }
